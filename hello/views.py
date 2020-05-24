593993f6c946cd2bfa07ad883e01d3e746c914b8
Daniel Pinon <danielpinon@danielpinon.com.br>
2020-05-23 23:18:42 -0300

    Feat: Melhorias

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from selenium import webdriver
+import os
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--remote-debugging-address=0.0.0.0")
+chrome_options.add_argument("--remote-debugging-port=9222")
+#chrome_options.addArguments("load-extension=/path/to/extension");
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# Now you can start using Selenium
+driver.get("https://web.whatsapp.com/")
+
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse(driver.page_source)
     #return render(request, "index.html")
 
 
